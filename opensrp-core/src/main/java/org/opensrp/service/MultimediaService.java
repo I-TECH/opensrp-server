bcbb93d36759db1a52ac0d1996d3ffb3220e8e16
Amos Laboso <alaboso@itech-kenya.org>
2017-04-27 15:13:45 +0300

    Update java class with merge conflicts

--- opensrp-core/src/main/java/org/opensrp/service/MultimediaService.java
+++ opensrp-core/src/main/java/org/opensrp/service/MultimediaService.java
@@ -15,18 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class MultimediaService {
-<<<<<<< HEAD
-	private static Logger logger = LoggerFactory.getLogger(MultimediaService.class
-			.toString());
-=======
     private static Logger logger = LoggerFactory.getLogger(MultimediaService.class.toString());
-<<<<<<< HEAD
->>>>>>> 719c92e27338c071e47ad4e67af50a5a0b9402df
-
-=======
     public static final String IMAGES_DIR="images";
     private static final String VIDEOS_DIR="videos";
->>>>>>> upstream/path
 	private final MultimediaRepository multimediaRepository;
 	private String multimediaDirPath;
 	@Value("#{opensrp['multimedia.directory.name']}")
@@ -34,35 +25,21 @@ public class MultimediaService {
 	
 
 	@Autowired
-<<<<<<< HEAD
-	public MultimediaService(MultimediaRepository multimediaRepository, @Value("#{opensrp['multimedia.directory.name']}") String multimediaDirName) {
-=======
 	public MultimediaService(MultimediaRepository multimediaRepository) {
->>>>>>> 719c92e27338c071e47ad4e67af50a5a0b9402df
 		this.multimediaRepository = multimediaRepository;
-		this.multimediaDirPath = multimediaDirName;
 	}
 
 	public String saveMultimediaFile(MultimediaDTO multimediaDTO, MultipartFile file) {
 		
 		boolean uploadStatus = uploadFile(multimediaDTO, file);
-         
-		String[] multimediaDirPathSplit =  multimediaDirPath.split("/", 3);
-		String multimediaDirPathDB = File.separator + multimediaDirPathSplit[2];
-		
+
 		if (uploadStatus) {
 			try {
-				logger.info("Image path : " + multimediaDirPath);
-				
 				Multimedia multimediaFile = new Multimedia()
 						.withCaseId(multimediaDTO.caseId())
 						.withProviderId(multimediaDTO.providerId())
 						.withContentType(multimediaDTO.contentType())
-<<<<<<< HEAD
-						.withFilePath(multimediaDirPathDB)
-=======
 						.withFilePath(multimediaDTO.filePath())
->>>>>>> 719c92e27338c071e47ad4e67af50a5a0b9402df
 						.withFileCategory(multimediaDTO.fileCategory());
 
 				multimediaRepository.add(multimediaFile);
@@ -80,49 +57,6 @@ public class MultimediaService {
 
 	public boolean uploadFile(MultimediaDTO multimediaDTO,
 			MultipartFile multimediaFile) {
-<<<<<<< HEAD
-		 
-		if (!multimediaFile.isEmpty()) {
-			try {
-
-				 multimediaDirPath += File.separator + multimediaDTO.providerId()+ File.separator;
-
-				switch (multimediaDTO.contentType()) {
-				
-				case "application/octet-stream":
-					String videoDirPath = multimediaDirPath += "videos";
-					makeMultimediaDir(videoDirPath);
-					multimediaDirPath += File.separator
-							+ multimediaDTO.caseId() + ".mp4";
-					break;
-
-				case "image/jpeg":
-					String jpgImgDirPath = multimediaDirPath += "images";
-					makeMultimediaDir(jpgImgDirPath);
-					multimediaDirPath += File.separator
-							+ multimediaDTO.caseId() + ".jpg";
-					break;
-
-				case "image/gif":
-					String gifImgDirPath = multimediaDirPath += "images";
-					makeMultimediaDir(gifImgDirPath);
-					multimediaDirPath += File.separator
-							+ multimediaDTO.caseId() + ".gif";
-					break;
-
-				case "image/png":
-					String pngImgDirPath = multimediaDirPath += "images";
-					makeMultimediaDir(pngImgDirPath);
-					multimediaDirPath += File.separator
-							+ multimediaDTO.caseId() + ".png";
-					break;
-
-				default:
-					String defaultDirPath = multimediaDirPath += "images";
-					makeMultimediaDir(defaultDirPath);
-					multimediaDirPath += File.separator
-							+ multimediaDTO.caseId() + ".jpg";
-=======
 		
 		// String baseMultimediaDirPath = System.getProperty("user.home");
 
@@ -151,7 +85,6 @@ public class MultimediaService {
 				case "image/png":
 					multimediaDirPath += IMAGES_DIR; 
 					fileExt=".png";
->>>>>>> 719c92e27338c071e47ad4e67af50a5a0b9402df
 					break;
 
 				}
@@ -180,13 +113,7 @@ public class MultimediaService {
 			return false;
 		}
 	}
-    private void makeMultimediaDir(String dirPath)
-    {
-    	File file = new File(dirPath);
-		 if(!file.exists())
-			 file.mkdirs();
-			 
-    }
+
 	public List<Multimedia> getMultimediaFiles(String providerId) {
 		return multimediaRepository.all(providerId);
 	}
